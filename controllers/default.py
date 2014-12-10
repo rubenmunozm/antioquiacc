6a63102f194630ec80868633940e383ff7695e5d
rubenmunozm <ruben.mu.mo@gmail.com>
2014-12-10 03:40:57 -0700

    se agrego link a sinai, y todo lo referente a la seccion de noticias

--- controllers/default.py
+++ controllers/default.py
@@ -16,25 +16,27 @@ from gluon.debug import dbg
 #dbg.set_trace() # stop here!
 
 def index():
-  if request.args(0):
-      now = int(time.time())
-      cakeToken = jwt.encode(
-      {
-      "iss":"07698772005655753885",
-      "aud":"Google",
-      "typ":"google/payments/inapp/item/v1",
-      "iat":now,
-      "exp":now + 3600,
-      "request":{
-        "currencyCode":"USD",
-        "price":request.args(0),
-        "name":"Donativo",
-        "sellerData":"Antioquia Calvary Chapel",
-        "description":"Donación a Iglesia Antioquia Calvary Chapel"
-        }
-      },"Th-H-LlhHhn9jB9_c_aV3A")
-  else:
-    pass
+  noticia = db(db.noticias).select().last()
+
+  # if request.args(0):
+  #     now = int(time.time())
+  #     cakeToken = jwt.encode(
+  #     {
+  #     "iss":"07698772005655753885",
+  #     "aud":"Google",
+  #     "typ":"google/payments/inapp/item/v1",
+  #     "iat":now,
+  #     "exp":now + 3600,
+  #     "request":{
+  #       "currencyCode":"USD",
+  #       "price":request.args(0),
+  #       "name":"Donativo",
+  #       "sellerData":"Antioquia Calvary Chapel",
+  #       "description":"Donación a Iglesia Antioquia Calvary Chapel"
+  #       }
+  #     },"Th-H-LlhHhn9jB9_c_aV3A")
+  # else:
+  #   pass
   
   form = SQLFORM(db.mensajes)
   if form.process(session=None, formname='contact_form').accepted:
@@ -44,31 +46,70 @@ def index():
   return locals()
 
 def index_en():
-  if request.args(0):
-      now = int(time.time())
-      cakeToken = jwt.encode(
-      {
-      "iss":"07698772005655753885",
-      "aud":"Google",
-      "typ":"google/payments/inapp/item/v1",
-      "iat":now,
-      "exp":now + 3600,
-      "request":{
-        "currencyCode":"USD",
-        "price":request.args(0),
-        "name":"Donation",
-        "sellerData":"Antioquia Calvary Chapel",
-        "description":"Gift to Antioquia Calvary Chapel church"
-        }
-      },"Th-H-LlhHhn9jB9_c_aV3A")
-  else:
-    pass
+  noticia = db(db.noticias).select().last()
+  # if request.args(0):
+  #     now = int(time.time())
+  #     cakeToken = jwt.encode(
+  #     {
+  #     "iss":"07698772005655753885",
+  #     "aud":"Google",
+  #     "typ":"google/payments/inapp/item/v1",
+  #     "iat":now,
+  #     "exp":now + 3600,
+  #     "request":{
+  #       "currencyCode":"USD",
+  #       "price":request.args(0),
+  #       "name":"Donation",
+  #       "sellerData":"Antioquia Calvary Chapel",
+  #       "description":"Gift to Antioquia Calvary Chapel church"
+  #       }
+  #     },"Th-H-LlhHhn9jB9_c_aV3A")
+  # else:
+  #   pass
 
   form = SQLFORM(db.mensajes)
   if form.process(session=None, formname='contact_form').accepted:
       response.flash = 'form accepted'
   elif form.errors:
       response.flash = 'form has errors'
+  return locals()
+
+def noticia():
+  try:
+    noticia = db(db.noticias.id==request.vars.id).select().first()
+  except Exception, e:
+    pass
+  rows = db(db.noticias).select(db.noticias.id,db.noticias.titulo,db.noticias.fecha, orderby=~db.noticias.id, limitby=(0,5))
+  return locals()
+
+def noticias():
+  noticia = db(db.noticias).select().last()
+  if not request.vars.start:
+    ini=0
+  else:
+    ini=int(request.vars.start)
+
+  fin=ini+4
+  rows = db(db.noticias).select(db.noticias.ALL, orderby=~db.noticias.id, limitby=(ini,fin))
+  return locals()
+
+def new():
+  try:
+    noticia = db(db.noticias.id==request.vars.id).select().first()
+  except Exception, e:
+    pass
+  rows = db(db.noticias).select(db.noticias.id,db.noticias.titulo,db.noticias.fecha, orderby=~db.noticias.id, limitby=(0,5))
+  return locals()
+
+def news():
+  noticia = db(db.noticias).select().last()
+  if not request.vars.start:
+    ini=0
+  else:
+    ini=int(request.vars.start)
+
+  fin=ini+4
+  rows = db(db.noticias).select(db.noticias.ALL, orderby=~db.noticias.id, limitby=(ini,fin))
   return locals()
 
 def web():
